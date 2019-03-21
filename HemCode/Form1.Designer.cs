7f030ddcc4de5c1ae356984743d31a836f05d1aa
patty98 <linascabeskrovnaya@gmail.com>
2019-03-21 13:39:32 +0300

    1

---
+++ HemCode/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace HemCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(100, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 334);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 27);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Кодировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "m=";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(44, 147);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(361, 129);
+            this.panel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "          ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "         ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ HemCode/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HemCode
+{
+    public partial class Form1 : Form
+    {
+        int n;
+        int k;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+        private void Code()
+        {
+            StringBuilder mes = ParamCode();
+            int n1 = mes.Length;
+
+            int[,] matrix = MatrixCode(k,n);
+            for (int i = 0; i < k; i++)
+            {
+
+                for (int j = 0; j < n; j++)
+                {
+
+                   
+                }
+            }
+            int[] coef = new int[k];
+            int count = 0;
+            for(int i=0;i<k;i++)
+            {
+              count = 0;
+                for(int j=0;j<n;j++)
+                {
+                    
+                    if(matrix[i,j]==1 && mes[j]!='k')
+                    {
+                        count++;
+                        if (count == 1)
+                        {
+                            coef[i] = Convert.ToInt16(mes[j]);
+
+                        }
+                        else
+                        {
+                            coef[i] ^= Convert.ToInt16(mes[j]);
+                        }
+
+                    }
+                }
+            }
+
+            for(int i=0;i<k;i++)
+            {
+                int y = (int)Math.Pow(2, i);
+                if(mes[y-1]=='k')
+                {
+                    mes[y-1] = Convert.ToChar(coef[i]);
+                }
+            }
+            for (int i = 0; i < k; i++)
+            {
+                if(coef[i]==48)
+                {
+                    coef[i] = 0;
+                }
+                else
+                {
+                    coef[i] = 1;
+                }
+            }
+            StringBuilder mes_error = Error(mes);
+            int[] sind = new int[k];
+            for (int i = 0; i < k; i++)
+            {
+                count = 0;
+                sind[i] = coef[i];
+                for (int j = 0; j < n; j++)
+                {
+                    
+                    if (matrix[i, j] == 1 && j!=Math.Pow(2,i)-1)
+                    {
+                        sind[i] ^= mes_error[j] - 48; 
+                       
+
+                    }
+                }
+            }
+            StringBuilder bit_error = new StringBuilder();
+            for (int i=0;i<k; i++)
+            {
+              
+                bit_error.Append(sind[k-i-1]);
+
+            }
+            int c = 0;
+            int bit = Convert.ToInt16(bit_error.ToString(), 2);
+            foreach(var value in sind)
+            {
+                if(value==0)
+                {
+                    c++;
+                }
+            }
+            
+                if (c==k)
+            {
+               
+               label1.Text = "Message transferred without errors.("+mes +")";
+
+             }
+            else
+            {
+                label1.Text = "Message transferred with errors in " + bit.ToString() + " bit (" + mes_error + ")";
+              if(mes[bit-1]=='0')
+                {
+                mes_error[bit-1] = '1';
+                }
+              if(mes[bit-1]=='1')
+                 {
+                mes_error[bit-1] = '0';
+                 }
+                label3.Text = "\n Error was fixed: " + mes_error;
+
+            }
+            
+            
+        }
+        private StringBuilder ParamCode()
+        {
+            int m = Convert.ToInt16(textBox1.Text);
+            string g = Convert.ToString(m, 2);
+            StringBuilder mes = new StringBuilder();
+            mes.Append(g);
+            
+            int n1 = mes.Length;
+            k = 0;
+
+            while(k<Math.Log(k+n1+1,2))
+            {
+                k++;
+            }
+             n = k + n1;
+
+            int pow = 0;
+            int i = 0;
+            for (i = 0; ; i++)
+            {
+
+                pow = (int)Math.Pow(2, i);
+                if (pow >= n)
+                {
+                    i--;
+                    break;
+                }
+                else
+                {
+                    string s = 'k'.ToString();
+                    mes = mes.Insert(pow-1, s);
+                }
+
+
+            }
+            return mes;
+        }
+        private int [,] MatrixCode(int k,int n)
+        {
+
+            int[,] matrix = new int[k,n];
+            for(int s=0;s<k;s++)
+            {
+                for (int r=0;r<n;r++)
+                {
+                    matrix[s, r] = 0;
+
+                }
+                
+            }
+
+            int i = 0;
+            int x = 0;
+            int w = 0;
+            int count = 0;
+            while (i < k)
+            {
+
+              
+               int y = Convert.ToInt16(Math.Pow(2, i));
+                w = 0;
+                while ((y - 1 + w) <n)
+                {
+                    matrix[i, y - 1 + w] = 1;
+
+
+                    for (int j = 1; j < y && (y - 1 + j) < n; j++)
+                    {
+                        matrix[i, y - 1+ w + j] = 1;
+                        count++;
+                    }
+
+                    w += y + 1 + count;
+
+                    count = 0;
+
+
+                }
+                i++;
+               
+                
+            }
+
+            return matrix;
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private StringBuilder Error(StringBuilder mes)
+        {
+            Random rnd = new Random();
+            int index = rnd.Next(0, n);
+            if(mes[index]=='0')
+            {
+                mes[index] = '1';
+            }
+            else
+            {
+                mes[index] = '0';
+            }
+            return mes;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Code();
+        }
+    }
+}
